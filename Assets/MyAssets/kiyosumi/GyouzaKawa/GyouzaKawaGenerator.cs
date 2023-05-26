457e152e27a96b921a2794e19af25387062e9f18
kiyosumi874 <shiiharakiyosumi@gmail.com>
2023-05-27 07:11:37 +0900

    餃子の皮をランダムな時間でいろいろな種類を飛ばすようになった。
    次は皮の種類を増やし投げる人を表示する

--- Assets/MyAssets/kiyosumi/GyouzaKawa/GyouzaKawaGenerator.cs
+++ Assets/MyAssets/kiyosumi/GyouzaKawa/GyouzaKawaGenerator.cs
@@ -4,19 +4,35 @@ using UnityEngine;
 
 public class GyouzaKawaGenerator : MonoBehaviour
 {
-    [SerializeField] private GameObject gyouzaKawaPrefab;
+    [SerializeField] private List<GameObject> gyouzaKawaPrefabs;
+    private float nowTime;
+    private float nextTime;
+    private int prefabIndex;
+    private int prefabSize;
+    [SerializeField] private float randTimeMax = 6.0f;
+    [SerializeField] private float randTimeMin = 3.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        nowTime = 0.0f;
+        nextTime = Random.Range(randTimeMin, randTimeMax);
+        prefabSize = gyouzaKawaPrefabs.Count;
+        prefabIndex = Random.Range(0, prefabSize);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Return)) 
+        if (nowTime > nextTime)
         {
-            Instantiate(gyouzaKawaPrefab);
+            nowTime = 0.0f;
+            nextTime = Random.Range(randTimeMin, randTimeMax);
+            Instantiate(gyouzaKawaPrefabs[prefabIndex], this.transform);
+            prefabIndex = Random.Range(0, prefabSize);
         }
+        Debug.Log(nowTime);
+        Debug.Log(prefabIndex);
+        Debug.Log(prefabSize);
+        nowTime += Time.deltaTime;
     }
 }
