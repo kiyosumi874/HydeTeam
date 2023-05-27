5fa590616c0958e4efddc9b46402f7c2c8a79312
YamaokaShinsaku <20010017ys@edu.fca.ac.jp>
2023-05-27 21:22:49 +0900

    トレーラー版の作成
    Sceneフォルダ内の TrailerScene がトレーラー版のシーンです。
    トレーラー版に使用した変更を加えたオブジェクトはPrefabフォルダ内に入れてます。
    
    変更したもの
    ・GyouzaKawa : タグをつけた
    ・Meat : タグをつけた、当たり判定を付けた

---
+++ Assets/Yamaoka/Script/HitCheck.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitCheck : MonoBehaviour
+{
+    /// <summary>
+    /// 当たり判定
+    /// </summary>
+    /// <param name="collision">当たったオブジェクト</param>
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Kawa タグを持つオブジェクトに当たったとき
+        if(collision.gameObject.tag == "Kawa")
+        {
+
+            Debug.Log("Hit!!");
+            // 自分を削除する
+            Destroy(this.gameObject);
+        }
+    }
+}
