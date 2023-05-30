246c789e51197e614f42f7c62f6bf43690011dca
kiyosumi874 <shiiharakiyosumi@gmail.com>
2023-05-30 22:05:43 +0900

    焼き餃子出現!

--- Assets/MyAssets/kiyosumi/GyouzaKawa/GyouzaKawaMove0.cs
+++ Assets/MyAssets/kiyosumi/GyouzaKawa/GyouzaKawaMove0.cs
@@ -6,9 +6,12 @@ using UnityEngine;
 public class GyouzaKawaMove0 : MonoBehaviour
 {
     [SerializeField] private bool isLeftDir = true;
-    [SerializeField] private GameObject gyouzaPrefab;
+    [SerializeField] private Sprite yakiGyouza;
+    [SerializeField, Range(0.0f, 10.0f)] private float deleteTime = 1.0f;
     [SerializeField,Range(0.0f,10.0f)] private float power = 1.0f;
     [SerializeField,Range(0,10)] private float randomPower = 0;
+    private bool isCombination = false; // ”ç‚Æ“÷‚¾‚Ë‚ª‡‘Ì‚µ‚½‚çtrue
+    private float time = 0.0f; // ‡‘Ì‚µ‚Ä‚©‚ç‚ÌŠÔ
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +21,17 @@ public class GyouzaKawaMove0 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // ‡‘Ì‚µ‚½‚çˆÚ“®‚µ‚È‚¢
+        if (isCombination)
+        {
+            time += Time.deltaTime;
+            if (deleteTime < time)
+            {
+                Destroy(this.gameObject);
+            }
+            return;
+        }
+
         // ¶•ûŒü‚É”ò‚Î‚·ê‡
         if (isLeftDir)
         {
@@ -48,8 +62,10 @@ public class GyouzaKawaMove0 : MonoBehaviour
 
         if (collision.CompareTag("Meet"))
         {
-            Instantiate(gyouzaPrefab, this.gameObject.transform);
-            //Destroy(this.gameObject);
+            isCombination = true;
+            GetComponent<SpriteRenderer>().sprite = yakiGyouza;
+            GetComponent<CircleCollider2D>().enabled = false;
+            transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
         }
     }
 }
