42e3e73984221184ec5bca523221b2bc785affc6
YamaokaShinsaku <20010017ys@edu.fca.ac.jp>
2023-06-05 11:22:45 +0900

    新しいMeetGeneratorの作成
    すでにある方を修正しようとしたらGitでエラーが出たので、新しいものを作成

---
+++ Assets/Yamaoka/Script/NewMeetGenerator.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class NewMeetGenerator : MonoBehaviour
+{
+    //変数宣言
+    public GameObject Meat; //肉だね
+    GameObject C_Meat; //コピーされた肉だね
+    const float span = 0.2f; //肉だねを出す間隔
+    float deltatime = 0; //デルタタイム
+    bool TapBottan = false; //ボタンが押されているかどうか
+    float MeatX = 0;
+    float MeatY = 4.0f;
+
+    [SerializeField]
+    private Transform createPosition;    // 肉だねを生成する座標
+
+    // Update is called once per frame
+    void Update()
+    {
+        //デルタタイム計測
+        this.deltatime += Time.deltaTime;
+
+        //ボタン感知
+        //Enterかspaceか左クリックでtrueにする
+        if (Input.GetMouseButton(0) || Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return))
+        {
+            TapBottan = true;
+        }
+        else
+        {
+            TapBottan = false;
+        }
+
+        //ボタンが押されているときに肉だねを落とす
+        if (TapBottan && deltatime >= span)　//spanとdeltatimeで落とす間隔を制御する
+        {
+            this.deltatime = 0;　//デルタタイムリセット
+            C_Meat = Instantiate(Meat); //コピーを生成生成
+            //コピーした肉だねの設定
+            C_Meat.transform.position = new Vector2(createPosition.position.x, createPosition.position.y); //場所
+            C_Meat.AddComponent<Rigidbody2D>(); //落下
+        }
+    }
+}
