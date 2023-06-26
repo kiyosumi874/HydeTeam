4d11d46a5a77551972f902e48d5b2ac102d1254a
koma2san <>
2023-06-26 10:32:32 +0900

    リザルトシーン修正

--- Assets/komatsu/Result Scene/GyouzaFall.cs
+++ Assets/komatsu/Result Scene/GyouzaFall.cs
@@ -10,6 +10,7 @@ public class GyouzaFall : MonoBehaviour
     float GyozaX = 0;                       //餃子を落とす位置のY座標
     const float GyozaY = 5.0f;              //餃子を落とす位置のY座標
     int Cnt = 0;                            //餃子を落とした数を数えるカウント
+    const int MaxGyouzaNum = 5;             //餃子の数の最大値
     float deltatime = 0;                    //デルタタイム
     const float span = 0.45f;               //餃子を落とすクールタイム
     public bool DrawscoreSetFlg = false;    //DrawScoreをするかどうかのフラグ
@@ -23,19 +24,22 @@ public class GyouzaFall : MonoBehaviour
     void Update()
     {
         //クールタイムが解除されたら指定された数だけ餃子を落とす
-        if (Cnt < GyouzaNum && deltatime > span)　
+        if (Cnt < MaxGyouzaNum && deltatime > span)　
         {
             //クールタイムリセット
             deltatime = 0;
-            //餃子を生成して落とす
-            c_Gyoza = Instantiate(Gyoza); //コピーを生成
-            GyozaX = Random.Range(-10.0f, 10.0f);//X軸の値をランダムで決める
-            c_Gyoza.transform.position = new Vector2(GyozaX, GyozaY); //場所
-            c_Gyoza.AddComponent<Rigidbody2D>(); //落下
+            if(Cnt < GyouzaNum)//餃子の数だけ落とす
+            {
+                //餃子を生成して落とす
+                c_Gyoza = Instantiate(Gyoza); //コピーを生成
+                GyozaX = Random.Range(-10.0f, 10.0f);//X軸の値をランダムで決める
+                c_Gyoza.transform.position = new Vector2(GyozaX, GyozaY); //場所
+                c_Gyoza.AddComponent<Rigidbody2D>(); //落下
+            }
             Cnt++;
         }
         //餃子をすべて落とし終わったらScoreDrawを実行する
-        else if(Cnt == GyouzaNum && deltatime > span)
+        else if(Cnt == MaxGyouzaNum && deltatime > span)
         {
             //Flgをtrueにする
             DrawscoreSetFlg = true;
